e3a637fb45d60836224c3650e5cba2dd3ec65927
06250505Yjh <2648216482@qq.com>
2022-01-02 09:34:14 +0800

    增加了左侧导航栏

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,11 +1,46 @@
 <template>
-  <div>
-    Home
-  </div>
+  <el-container>
+    <el-header>
+      <div>
+        <span>管理系统</span>
+      </div>
+      <el-button type="info">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          background-color="#0086b3"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 
 </script>
 <style lang="less" scoped>
+.el-header{
+  background: #55a532;
+  display:flex;
+  justify-content: space-between;
+  font-size:30px
+}
+.el-aside{
+  background: #0086b3;
+}
+.el-container{
+  width: 100%;
+  height: 100%;
+}
 
 </style>
